001abb512e511e5b55802dbea3791b152a500867
ge35kij <ge35kij@mytum.de>
2020-11-01 20:46:56 +0200

    api update

--- src/core/api.tsx
+++ src/core/api.tsx
@@ -9,9 +9,10 @@ import {
 import { WebSocketLink } from "@apollo/client/link/ws";
 import { getMainDefinition } from "@apollo/client/utilities";
 
-const api_base_http = process.env.API_BASE_HTTP || "http://localhost:8000/";
+const api_base_http =
+  process.env.REACT_APP_API_BASE_HTTP || "http://localhost:8000/";
 const api_base_ws =
-  process.env.API_BASE_WS || "ws://localhost:8000/subscriptions";
+  process.env.REACT_APP_API_BASE_WS || "ws://localhost:8000/subscriptions";
 
 const httpLink = new HttpLink({ uri: api_base_http });
 const wsLink = new WebSocketLink({
